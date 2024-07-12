b1534c8158a811970bbca2a6e5cbb61d88e8c71a
kizza <kieran.thorley@gmail.com>
2024-07-12 11:30:53 +0100

    heroku fixes

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,7 @@ DEBUG = ENV == 'DEV'
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://money-mapper-bd99db5b6ba8.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = [str(os.getenv('DEPLOY_URL'))]
 
 
 # Application definition
